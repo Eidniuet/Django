bf12d8a6f4690096f7e7b2f6f2b58a46d08ae809
Eidniuet <wenghongtong27@gmail.com>
2023-08-27 09:33:26 +0800

    ok

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nr*gn9hh*ubz8(@6b8n5d$wwom6d32s)rza3be3w4c+!!o9&_3'
+SECRET_KEY = 'django-insecure-uyr@wnp7x8@+26kpg8_r2emph(2@ty161@qk=ylo$yubrl3&d2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'item'
 ]
 
 MIDDLEWARE = [
